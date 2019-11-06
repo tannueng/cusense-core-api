0960ece54507392a264c5b81973bbc06dbfb62fe
tannueng <tannueng@gmail.com>
2019-11-06 22:30:37 +0700

    add regis/login for token

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 //Import Routes
 const queryRoute = require("./routes/query");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB")
+);
+
+//Middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Responding to root route");
-  res.send("Hello from ROOT");
+  res.send("CONNECTED TO API SERVER");
 });
 
 //Route Middleware
 app.use("/api/v1/check", queryRoute);
+app.use("/api/v1/users", authRoute);
 
 // localhost:3333
 app.listen(3333, () => {
