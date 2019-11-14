5e843030a8568e29b12b9ef7f2a2644b23256c35
tannueng <tannueng@gmail.com>
2019-11-15 03:56:22 +0700

    sql to match influx query

--- routes/query.js
+++ routes/query.js
@@ -45,7 +45,35 @@ router.get("/station", (req, res) => {
 
     console.log("Station data received from SQL");
     // rows[0].stationid
-    res.json(rows[0].stationid);
+
+    res.json(rows);
+  });
+});
+
+router.get("/active", (req, res) => {
+  con.query("SELECT * FROM station WHERE publish = 1", (err, rows) => {
+    if (err) throw err;
+    const final_result = {};
+    console.log("Station data received from SQL");
+    // rows[0].stationid
+    influx
+      .query(
+        "select mean(*) from ss2 where time > now() - 24h group by sensorid"
+      )
+      .then(results => {
+        for (i = 0; i < rows.length; i++) {
+          for (j = 0; j < results.length; i++) {
+            if (rows[i].stationid == results[j].stationid) {
+              row[i].info = results[j];
+              final_result += rows[i];
+              console.log("final_result", final_result);
+            }
+          }
+        }
+      })
+      .catch(console.error);
+
+    res.json(final_result);
   });
 });
 
