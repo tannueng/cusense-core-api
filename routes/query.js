02b15b3dc2a03a22ddfa4c1eda3647f8b6345dcf
tannueng <tannueng@gmail.com>
2019-12-18 20:32:41 +0700

    add /pmDay

--- routes/query.js
+++ routes/query.js
@@ -76,4 +76,33 @@ router.get("/active", (req, res) => {
   });
 });
 
+router.get("/pmDay", (req, res) => {
+  pool.query(
+    "SELECT stationid,project,id,lat,lon,name,province,tambol,amphoe FROM station WHERE publish = 1",
+    function(err, rows, fields) {
+      // Connection is automatically released when query resolves
+      console.log("Station data received from SQL");
+      influx
+        .query(
+          "select mean(pm1) as pm1, mean(pm25) as pm25, mean(pm10) as pm10 from ss2 where time > now() - 24h group by sensorid"
+        )
+        .then(results => {
+          let final_result = {};
+          for (i = 0; i < rows.length; i++) {
+            for (j = 0; j < results.length; j++) {
+              if (rows[i].stationid == results[j].sensorid) {
+                console.log("in loop matches");
+                final_result[rows[i].id] = results[j];
+                final_result[rows[i].id].info = rows[i];
+                console.log("final_result", final_result);
+              }
+            }
+          }
+          res.json(final_result);
+        })
+        .catch(console.error);
+    }
+  );
+});
+
 module.exports = router;
