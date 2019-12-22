72b39c8a230239c63cd5a599e8f61f975e3d4b46
tannueng <tannueng@gmail.com>
2019-12-22 18:25:42 +0700

    use manual query

--- routes/query.js
+++ routes/query.js
@@ -176,20 +176,48 @@ router.post("/byStation/:timeframe/:date", (req, res) => {
     const { error } = dateValidation(date);
     if (error) return res.status(400).send(error.details[0].message);
 
-    matchQuery(
-      defaultSQLquery,
-      "select mean(*) from airdata where time >= '2019-12-21' and time <= '2019-12-21' + 1d and \"topic\" = 'nansensor/CU-S0040' group by time(1h)"
+    pool.query(defaultSQLquery, function(err, rows, fields) {
+      influx
+        .query(
+          "select mean(*) from airdata where time >= '2019-12-21' and time <= '2019-12-21' + 1d and \"topic\" = 'nansensor/CU-S0040' group by time(1h)"
+        )
+        .then(results => {
+          let final_result = {};
+          let firstTime = true;
+          console.log(results);
+          for (i = 0; i < rows.length; i++) {
+            for (j = 0; j < results.length; j++) {
+              if (rows[i].topic == results[j].topic) {
+                if (firstTime) {
+                  final_result[rows[i].id] = {};
+                  final_result[rows[i].id].data = [];
+                }
+                final_result[rows[i].id].data.push(results[j]);
+                final_result[rows[i].id].info = rows[i];
+                firstTime = false;
+              }
+            }
+            firstTime = true;
+          }
+          console.log(final_result);
+          res.json(final_result);
+        })
+        .catch(console.error);
+    });
 
-      // "select mean(*) from airdata where time >= '" +
-      //   date +
-      //   "' and time <= '" +
-      //   date +
-      //   "' + 1d and  \"topic\" = '" +
-      //   topic +
-      //   "' group by time(1h)"
-        ,
-      res
-    );
+    // matchQuery(
+    //   defaultSQLquery,
+    //   "select mean(*) from airdata where time >= '2019-12-21' and time <= '2019-12-21' + 1d and \"topic\" = 'nansensor/CU-S0040' group by time(1h)",
+
+    //   // "select mean(*) from airdata where time >= '" +
+    //   //   date +
+    //   //   "' and time <= '" +
+    //   //   date +
+    //   //   "' + 1d and  \"topic\" = '" +
+    //   //   topic +
+    //   //   "' group by time(1h)"
+    //   res
+    // );
   } else {
     res.status(400).send("Invalid URL Parameter.");
   }
