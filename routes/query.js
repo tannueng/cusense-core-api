4732250a6f89169bdcc9972c9b823e25c4512bdb
tannueng <tannueng@gmail.com>
2019-12-22 13:23:44 +0700

    .data

--- routes/query.js
+++ routes/query.js
@@ -54,17 +54,17 @@ router.get("/stations/all", (req, res) => {
 router.post("/direct", (req, res) => {
   const { error } = directQueryValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
+
   pool.query(defaultSQLquery, function(err, rows, fields) {
     influx
       .query(req.body.query)
       .then(results => {
         let final_result = {};
-        let k=0;
+        let k = 0;
         for (i = 0; i < rows.length; i++) {
           for (j = 0; j < results.length; j++) {
             if (rows[i].topic == results[j].topic) {
-              final_result[rows[i].id].data[k] = results[j]; //Overwrites the old object
+              final_result[rows[i].id].data = results[j]; //Overwrites the old object
               final_result[rows[i].id].info = rows[i];
               k++;
             }
