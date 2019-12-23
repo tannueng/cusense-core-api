a002aca67c72401f008152221036e49d28066f96
tannueng <tannueng@gmail.com>
2019-12-23 19:55:46 +0700

    use multipleQuery

--- routes/temp.js
+++ routes/temp.js
@@ -43,7 +43,7 @@ router.get("/getpttdata", (req, res) => {
 });
 
 router.get("/getcudata", (req, res) => {
-  matchQuery(
+  matchMultipleQuery(
     byGroupSQLQuery("cusensor2"),
     "select mean(pm1) as pm1, mean(pm25) as pm25, mean(pm10) as pm10, mean(temp) as temp, mean(humid) as humid, mean(temp) as temp from airdata where time > now() - 1h and \"group\" = 'cusensor2' group by topic",
     "cusensor2",
@@ -68,6 +68,41 @@ function matchQuery(mysqlQuery, influxQuery, distinct, res) {
         for (i = 0; i < rows.length; i++) {
           for (j = 0; j < results.length; j++) {
             if (rows[i].topic == distinct) {
+              if (firstTime) {
+                final_result[rows[i].id] = {};
+                final_result[rows[i].id].data = [];
+              }
+              final_result[rows[i].id].data.push(results[j]);
+              final_result[rows[i].id].info = rows[i];
+              firstTime = false;
+            }
+          }
+          firstTime = true;
+        }
+
+        res.json(final_result);
+      })
+      .catch(console.error);
+  });
+}
+
+function matchMultipleQuery(mysqlQuery, influxQuery, distinct, res) {
+  pool.query(mysqlQuery, function(err, rows, fields) {
+    influx
+      .query(influxQuery)
+      .then(results => {
+        let final_result = {};
+        let firstTime = true;
+        console.log(rows);
+        console.log(results);
+
+        if (results == "") {
+          res.send("No data for " + distinct + " for the last 1 hour.");
+        }
+
+        for (i = 0; i < rows.length; i++) {
+          for (j = 0; j < results.length; j++) {
+            if (rows[i].topic == results[j].topic) {
               if (firstTime) {
                 final_result[rows[i].id] = {};
                 final_result[rows[i].id].data = [];
