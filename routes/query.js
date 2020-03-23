e8d27f9bb64e585f6605d097638c3d57d1b6cdb1
tannueng <tannueng@gmail.com>
2020-03-23 22:21:52 +0700

    log time 3

--- routes/query.js
+++ routes/query.js
@@ -424,6 +424,7 @@ function matchSpecificQuery(mysqlQuery, influxQuery, topic, res) {
 
                 if (results[j].time != null) {
                   console.log("before ", results[j].time);
+                  console.log("before2 ", results[j].time.toString());
                   results[j].time = results[j].time.toLocaleString("en-US", {
                     timeZone: "Asia/Bangkok"
                   });
