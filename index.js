b9d9ff9d62e5f2c1a2e21b8a1c6319d4734ed4fb
tannueng <tannueng@gmail.com>
2020-04-25 22:10:59 +0700

    terminate old api + log old api usage + disable cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 // const https = require("https");
-// const fs = require("fs");
+const fs = require("fs");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
+const morgan = require("morgan");
+var path = require("path");
 
 // const port = 3333;
 
@@ -41,6 +43,10 @@ const config = {
   maxAge: 3600,
 };
 
+accessLogStream = fs.createWriteStream(path.join(__dirname, "old_access.log"), {
+  flags: "a",
+});
+
 //Middleware
 app.use(express.json());
 // app.use(cors(config));
@@ -59,25 +65,37 @@ app.get("/heartbeat", (req, res) => {
   res.send("The core API service is running.");
 });
 
-app.use("/api", (req, res, next) => {
-  res.header(
-    "We-are-moving!",
-    "This API service will be terminated on 12 APR 2020. We are moving to the new URL. Check out https://cusense.net/portal/#!/apis/7663e426-e4e5-4cee-a3e4-26e4e57cee4c/pages/9d46f643-4652-44eb-86f6-434652b4ebb0 for the new API documentation. The new baseURL is https://www.cusense.net:8082 use alongside with the new your own API Key."
-  );
-  next();
-});
+app.use(
+  "/api",
+  morgan("combined", { stream: accessLogStream }),
+  (req, res, next) => {
+    morgan(
+      ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'
+    );
+    res.header(
+      "We-moved!",
+      "This API service terminated on 12 APR 2020. We moved to the new URL. Checkout https://cusense.net/portal/#!/apis/7663e426-e4e5-4cee-a3e4-26e4e57cee4c/pages/9d46f643-4652-44eb-86f6-434652b4ebb0 for the new API documentation. The new baseURL is https://www.cusense.net:8082 use alongside with the new your own API Key."
+    );
 
-//Route Middleware
-app.use("/api/v1/stationInfo", stationRoute);
-app.use("/api/v1/manageStations", manageStationsRoute);
-app.use("/api/v1/sensorData", cors(config), queryRoute);
+    res
+      .status(404)
+      .send(
+        "This API service terminated on 12 APR 2020.\nWe moved to the new URL.\nCheckout https://cusense.net/portal/#!/apis/7663e426-e4e5-4cee-a3e4-26e4e57cee4c/pages/9d46f643-4652-44eb-86f6-434652b4ebb0 for the new API documentation.\nThe new baseURL is https://www.cusense.net:8082 use alongside with the new your own API Key."
+      );
+  }
+);
+
+//Original Route Middleware
+// app.use("/api/v1/stationInfo", stationRoute);
+// app.use("/api/v1/manageStations", manageStationsRoute);
+// app.use("/api/v1/sensorData", cors(config), queryRoute);
 // app.use("/api/v1/users", authRoute);
-app.use("/api/", tempRoute);
+// app.use("/api/", tempRoute);
 
 //New Route Middleware
 app.use("/v1/stationInfo", stationRoute);
 app.use("/v1/manageStations", manageStationsRoute);
-app.use("/v1/sensorData", cors(config), queryRoute);
+app.use("/v1/sensorData", queryRoute);
 
 localhost: 3333;
 app.listen(3333, () => {
