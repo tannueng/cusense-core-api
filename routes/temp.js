2666758abdab6792c40258e6601e29053d3cd48e
tannueng <tannueng@gmail.com>
2020-05-01 20:35:24 +0700

    rm send

--- routes/temp.js
+++ routes/temp.js
@@ -87,7 +87,6 @@ router.get("/covid/check/:id", function (req, res) {
         // errors.email = "User not found";
         res
           .status(200)
-          .send("ไม่พบความเสี่ยง\nหมายเลขบัตรประชาชน " + id)
           .json({ status: "ไม่พบความเสี่ยง", หมายเลขบัตรประชาชน: id });
         // stop further execution in this callback
       }
