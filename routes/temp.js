3b166d11077691544c9c2b1cc7034cecf926f98b
tannueng <tannueng@gmail.com>
2020-05-01 20:28:47 +0700

    fix res

--- routes/temp.js
+++ routes/temp.js
@@ -36,8 +36,9 @@ router.get("/covid/check/:id", function (req, res) {
   let foundcovid = false;
 
   if (!Number.isInteger(id) && id.length != 13) {
-    res.send("หมายเลขบัตรประชาชนไม่ถูกต้อง");
-    res.sendStatus(400).end();
+    res
+      .status(400)
+      .json({ status: 400, errors: "หมายเลขบัตรประชาชนไม่ถูกต้อง" });
   }
   fs.createReadStream("/home/api/files/traveller_list_01.csv")
     .pipe(
