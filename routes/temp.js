6ceaa64081d2a727e294740077b8ec217493eba0
tannueng <tannueng@gmail.com>
2020-05-01 20:25:55 +0700

    put send before

--- routes/temp.js
+++ routes/temp.js
@@ -36,7 +36,8 @@ router.get("/covid/check/:id", function (req, res) {
   let foundcovid = false;
 
   if (!Number.isInteger(id) && id.length != 13) {
-    res.sendStatus(400).send("หมายเลขบัตรประชาชนไม่ถูกต้อง").end();
+    res.send("หมายเลขบัตรประชาชนไม่ถูกต้อง");
+    res.sendStatus(400).end();
   }
   fs.createReadStream("/home/api/files/traveller_list_01.csv")
     .pipe(
